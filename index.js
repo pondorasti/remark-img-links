528c6ca661f660a7d1e104c5a1ded1b85c70e8a4
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-05-04 01:29:06 -0700

    Add module functionality

---
+++ index.js
@@ -0,0 +1,17 @@
+import visit from "unist-util-visit"
+
+module.exports = function (options) {
+  if (!options || !options.absolutePath) {
+    throw Error("Missing required `absolutePath` option.")
+  }
+
+  function visitor(node) {
+    node.url = options.absolutePath + node.url
+  }
+
+  function transform(tree) {
+    visit(tree, "image", visitor)
+  }
+
+  return transform
+}
