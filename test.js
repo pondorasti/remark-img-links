27e689eb4fd7d28dcf08bcd14d71ef617ae7525a
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-05-05 07:37:11 -0700

    Add new test

--- test.js
+++ test.js
@@ -4,13 +4,22 @@ const html = require("remark-html")
 const imgLinks = require(".")
 
 test("remark-img-links", function (t) {
-  t.plan(1)
+  t.plan(2)
 
   remark()
     .use(imgLinks, { absolutePath: "https://cdn.domain.com/" })
     .use(html)
     .process("![Screenshot](images/screenshot.png)", (err, file) => {
       const expectedYield = '<p><img src="https://cdn.domain.com/images/screenshot.png" alt="Screenshot"></p>\n'
+      t.equal(String(file), expectedYield)
+    })
+
+  remark()
+    .use(imgLinks, { absolutePath: "https://cdn.domain.com/" })
+    .use(html)
+    .process("![Screenshot](https:github.com/images/screenshot.png)", (err, file) => {
+      const expectedYield =
+        '<p><img src="https://cdn.domain.com/github.com/images/screenshot.png" alt="Screenshot"></p>\n'
       t.equal(String(file), expectedYield)
     })
 
